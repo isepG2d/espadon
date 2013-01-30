d92169b1085e27616eacca637a66682defeb6b3b
Vercier Hadrien <hadrien.vercier@gmail.com>
2013-01-30 14:21:52 +0100

    mon premier commit

---
+++ index2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html >
+<head>
+ <title>Mon super site</title>
+ <meta charset="utf-8" />
+</head>
+
+<body>
+
+  <div id="corps">
+   <h1>Mon super site</h1>
+
+   <p>
+     Bienvenue sur mon super site !<br />
+
+   </p>
+
+             <?php $maVariable=12;
+             echo 'la valeur de ma variable est de'  .$maVariable;
+             ?>
+           </div>
+
+
+  </body>
+
+
+
+
+</html>
